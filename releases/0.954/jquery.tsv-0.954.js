17f478b1deb9cc0544396a39519111854a403262
Bob Kerns <rwk@acm.org>
2012-09-09 19:12:11 -0700

    Release 0.954

--- releases/0.954/jquery.tsv-0.954.js
+++ releases/0.954/jquery.tsv-0.954.js
@@ -41,7 +41,7 @@
     }
 
     $.tsv = {
-	version: "0.954-git",
+	version: "0.954",
             /**
              * The default set of options. It is not recommended to change these, as the impact will be global
              */
