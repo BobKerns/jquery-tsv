d93c24b0fc393f9fd347cdc5b58fb68247804846
Bob Kerns <rwk@acm.org>
2012-09-09 19:23:02 -0700

    Release 0.955

--- releases/0.955/jquery.tsv-0.955.js
+++ releases/0.955/jquery.tsv-0.955.js
@@ -41,7 +41,7 @@
     }
 
     $.tsv = {
-	version: "0.955-git",
+	version: "0.955",
             /**
              * The default set of options. It is not recommended to change these, as the impact will be global
              */
